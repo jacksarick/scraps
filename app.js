a29126f24339f914ddde5ae80e2bccb04aa3a907
Jack Sarick <jacksarick@gmail.com>
2016-11-13 00:11:34 -0500

    Page building includes work. Still working on variables

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var   http = require('http');
 const PORT = 8080;
 
-const compose = require("./src/page-builder.js")
+var compose = require("./src/page-builder.js")(__dirname + "/pages/");
 
 function handler(request, response){
-	const url = (request.url == "/")? "/index.html" : request.url;
-	response.end(compose(__dirname + "/pages" + url));
+	const url = (request.url == "/")? "index.html" : request.url;
+	response.end(compose(url, {"variable":"This is some dope content!!!!"}));
 }
 
 var server = http.createServer(handler);
