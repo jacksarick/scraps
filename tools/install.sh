f1285a1072b227844f2f86cf9a0bf4b6e2529899
jacksarick <jacksarick@gmail.com>
2017-07-30 01:14:55 -0600

    Added install script

---
+++ tools/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd "${0%/*}" # Go to this directory
+cp config.json ..
+
+read -p "Generate dummy certs? (Y/N)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    ./gen-cert.sh
+    echo "You'll need to manually activate in config.json"
+fi
+
+cd ..
+mkdir db
+
+echo "Done!"
