1087151946efdd2eda98f7c1372ee4e86dbc5b2d
jacksarick <jacksarick@gmail.com>
2017-07-30 01:15:57 -0600

    Moved things around

--- gen-cert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir cert
-cd cert
-openssl genrsa -out private-key.pem 4096
-openssl req -new -key private-key.pem -out csr.pem
-openssl x509 -req -in csr.pem -signkey private-key.pem -out public-cert.pem
