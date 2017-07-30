0afb191d2362441535f7f85f7666b32bf29a6031
jacksarick <jacksarick@gmail.com>
2017-07-30 01:21:23 -0600

    Added host logging

--- src/https-server.js
+++ src/https-server.js
@@ -4,6 +4,7 @@ const log 	= require("./log.js");
 
 
 const config = require("../config.json");
+const HOST = config.host;
 const PORT = config.port;
 
 function server_generator(handler) {
@@ -13,7 +14,7 @@ function server_generator(handler) {
 	};
 
 	return server = https.createServer(options, handler).listen(PORT, function(){
-		log.sys("Listening on: https://localhost:" + PORT);
+		log.sys("Listening on: https://" + HOST + ":" + PORT);
 	});
 }
 
